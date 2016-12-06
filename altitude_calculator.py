04d0bfb20b2ffb8b42a470f2cdca36725a195e37
jasonscharff <jasonscharff@gmail.com>
2016-12-05 22:04:43 -0800

    Add code/docs/stuff

--- altitude_calculator.py
+++ altitude_calculator.py
@@ -46,7 +46,7 @@ def generate_combined_spreadsheet(geiger, pressure, gps, interior):
     interior_reader = csv.DictReader(open(interior))
 
 
-    #create a new file to use as the master concatination of all.
+    #create a new file to use as the master concatenation of all.
     with open ('master_unprocessed.csv', 'w') as new_file:
         #open this new file as a dictionary writer so rows can be written in the form of a dictionary.
         #the keys for this csv is the KEYS global variable.
@@ -98,20 +98,27 @@ def generate_combined_spreadsheet(geiger, pressure, gps, interior):
             writer.writerow(row)
 
 
+generate_combined_spreadsheet('geiger.csv', 'pressure.csv', 'gps.csv', 'interior.csv')
+
 #function to get altitude in meters from a given pressure using the NASA model
 #arg pressure, the pressure to get the altitude from in pascals
 def get_altitude_from_pressure(pressure):
     #convert pressure from pascals to kilopascals 
     pressure /= 1000
-    #the NASA model is a piecwise function
+    #the NASA model is a piecewise function
     #if pressure is above 22.7 kilopascals apply certain function to get altitude
     if pressure > 22.707:
+        #apply pressure function for pressure > 22.7 kilopascals.
+        #note that for relative altitude the 101.29 should be the initial pressure
+        #101.29 is just an approximation of sea level pressure from NASA.
         altitude = 44397.5-44388.3 * ((pressure/101.29) ** .19026)
-    #if the pressure is less than 2.48 kilopascals apply another function to get altitude
+    #if the pressure is less than 2.48 kilo pascals apply another function to get altitude
     elif pressure < 2.483:
+        #apply pressure function for <2.483 kilopascals
         altitude = 72441.47 * ((pressure/2.488) ** -.0878) - 47454.96
     #if pressure is between 2.48 and 22.7 kilopascals apply another function to get altitude
     else:
+        #apply pressure function for between 2.48 and 22.7 kilopascals.
         altitude = 11019.12 - 6369.43 * math.log(pressure/22.65)
     #return the altitude in meters
     return altitude
@@ -142,130 +149,263 @@ def add_altitude_if_pressure_present(filename):
                 #write the row into the new csv.
                 writer.writerow(row)
 
+add_altitude_temperature('master_unprocessed.csv')
 
-
-#precondition sorted by time
-def fillInMissingPoints(filename, measurement_key, output_name):
+#function to fill in missing data for a given column measurement_key in a
+#csv of the format outputted by generate_combined_spreadsheet.
+#arg, filename: the filename of the original file in the format of generate_combined_spreadsheet
+#that contains missing values in a specific column.
+#arg, measurement_key: the name of the column to fill in missing values of.
+#arg, output_name the name of the file to output with no values missing in the given column.
+#precondition: the CSV filename is sorted by time and all rows must have a time.
+def fill_in_missing_points(filename, measurement_key, output_name):
+    #open the original file
     with open(filename, 'r') as original_file:
+        #convert the original file into a list of dictionaries.
         reader = csv.DictReader(original_file)
+        #create a new file with the given filename
         with open (output_name, 'w') as new_file:
+            #open the newly created file as a CSV to write dictionary rows into it.
+            #use the global variable KEYS as the expected keys (the output of generate_combined_spreadsheet)
             writer = csv.DictWriter(new_file, KEYS)
+            #write the top row (column names) on the newly created CSV.
             writer.writeheader()
+            #initialize a list to store rows that need to be updated due to a missing value
+            #in the column measurement_key
             rows_missing_point = []
+            #initialize a variable previous_point to store the previously recorded value in the column measurement_key
             previous_point = 0
+            #initialize the a variable to store the time in which previous_point was taken.
+            #initialized to the max integer so that the first time on the CSV is closer to the time 
+            #for the first datapoint than the initialized value so the initialization value of
+            #previous_point is not used ever.
+            #this actually isn't necessary based on the values of the timestamps, but is a bit better practice.
             previous_time = sys.maxint
+            #iterate through the original file
             for row in reader:
+                #check if there is a value in the column measurement_key for the given row
                 if row[measurement_key] is not None and len(row[measurement_key]) > 0:
+                    #if there is a value set variable time equal to the time that measurement was taken.
                     time = float(row['time'])
+                    #iterate through a list of rows that need a value for the column measurement_key
                     for missing_row in rows_missing_point:
+                        #set the variable missing_time equal to the time the measurements in the row missing
+                        #a value for measurement_key were taken.
                         missing_time = float(missing_row['time'])
+                        #if the time that the measurements missing a value for measurement_key
+                        #were taken is closer to the time of the most for variable row
+                        #than for the previous occurrence of a value in the column measurement_key.
                         if time - missing_time <= previous_time - missing_time:
+                            #set the value in the row missing a value to the value in the current row
                             missing_row[measurement_key] = row[measurement_key]
-                        else:
+                        else: #time of the last found measurement is closer to the time for missing_row
+                            #set the value in the row missing a value to the previous occurrence of a value
+                            #in col measurement_key 
                             missing_row[measurement_key] = previous_point
 
+                    #set the previous measurement to the current one.
                     previous_point = row[measurement_key]
+                    #set the previous time to the time of previous_point.
                     previous_time = time
 
-
+                    #append the current row to the variable rows_missing_point before
+                    #writing to CSV.
                     rows_missing_point.append(row)
+                    #write everything from rows_missing_point into the CSV.
                     writer.writerows(rows_missing_point)
+                    #reset rows_missing_point.
                     rows_missing_point = []
-                else:
+                else: #no measurement of interest found
+                    #add the row into a list to populate with the next (or previous)
+                    #occurrence of a value in the column measurement_key.
                     rows_missing_point.append(row)
 
-def fillInMissingData():
+
+#function to fill in all columns completely for a CSV in the format
+#outputted by generate_combined_spreadsheet.
+#arg filename, the filename to fill in the columns for.
+#precondition: the CSV filename must be sorted by time and all rows must have a time.
+
+#this functiion is structured by applying fill_in_missing_points to each column.
+def fill_in_missing_data(filename):
+    #remove 'time' from the list of keys in a generate_combined_spreadsheet
+    #formatted file because time must be filled in for this to work
+    #(a precondition of this function)
     keys = list(filter(lambda x: x != 'time', KEYS))
+    #create a variable called count and set it equal to 0
+    #count will be used to generate intermediate CSV files
+    #with one column filled in at a time.
     count = 0
-    previous_filename = 'altitude_added.csv'
+    #create a variable called previous_filename to keep track of the name of intermediate files
+    #created by filling in one col at a time.
+    previous_filename = filename
     for key in keys:
+        #iterate through the list of keys to fill
+        #call the next file count.csv to keep track of progressively made files
+        #as each call to fill_in_missing_points fills only one column.
         new_filename = str(count) + '.csv'
-        fillInMissingPoints(previous_filename, key, new_filename)
+        #completely fill the column key and name the resulting file new_filename (just the count)
+        #and use the previous filename as the file to fill from so that progressive filling
+        #leads to all columns being filled.
+        fill_in_missing_points(previous_filename, key, new_filename)
+        #set the previous_filename equal to the filename of the newly created file
         previous_filename = new_filename
+        #increase the number of files created by 1. This count will then be be used for the next filename.
         count += 1
 
 
-#fillInMissingData()
-
-
-# def main():
-#     generate_combined_spreadsheet(geiger='geiger.csv', pressure='pressure.csv', interior='interior.csv', gps='gps.csv')
-#
-# main()
-#
-
-
-
-def fullyDeduplicateCSV(filename):
+#function to do strict deduplication on CSV filename.
+#removes any completely duplicate rows.
+#arg filename: the name of the file to deduplicate.
+def fully_deduplicate_csv(filename):
+    #open the file to deduplicate and create a new file called 
+    #deduped for the deduped version
     with open(filename, 'r') as in_file, open('deduped.csv', 'w') as out_file:
-        seen = set()  # set for fast O(1) amortized lookup
+        #create a set to keep track of previously seen rows.
+        #use a set over an array for efficient (O(1)) lookup.
+        seen = set() 
+        #iterate through the file to deduplicate line by line.
         for line in in_file:
+            #check if we haven't seen the line before.
             if line not in seen:
+                #add the line into the set seen to keep track
                 seen.add(line)
+                #write the line onto the new file.
                 out_file.write(line)
 
 
-
-def mostlyDedupCSV(filename):
+#function to deduplicate a CSV in the format
+#outputted by generate_combined_spreadsheet based on the value in column time.
+#arg filename: the name of the file to deduplicate
+def time_based_deduplicate_csv(filename):
+    #open the file to deduplicate and created a new file called time_deduped for the deduped version.
     with open(filename, 'r') as in_file, open('time_deduped.csv', 'w') as out_file:
+        #open the original file as a list of dictionaries
         reader = csv.DictReader(in_file)
+        #open the newly created file as a CSV with the columns equal to the global variable KEYS
+        #(the same as the output for generate_combined_spreadsheet)
         writer = csv.DictWriter(out_file, KEYS)
+        #write the header row (column names) to the newly created CSV
         writer.writeheader()
-        seen = set()  # set for fast O(1) amortized lookup
-        for row in reader:
+        #create a set to store viewed times.
+        #use a set for fast lookup
+        seen = set()  
+        for row in reader: #iterate through the original file
+            #set the variable time = to the value of column time in the original file
             time = row['time']
+            #check if this is the first time there has been an entry with the same timestamp
             if time not in seen:
+                #add the time to the set seen.
                 seen.add(time)
+                #write the row into the deduplicated CSV.
                 writer.writerow(row)
 
-#fullyDeduplicateCSV('imprecised_nondeduped.csv')
-#mostlyDedupCSV('deduped.csv')
-
-#generate_combined_spreadsheet('geiger.csv', 'pressure.csv', 'gps.csv', 'interior.csv')
-
-previous_altitude = -1
-
+#function to create a spreadsheet with the results from simultaneously collected
+#data points (sensors on the same Arduino) and altitude.
+#the output of this function will be a CSV just when measurements were taken
+#for the inputted keys and the altitude when those measurements were taken.
+#arg, filename: the filename for the sorted output of generate_combined_spreadsheet
+#arg, keys_to_include: the keys to add to the CSV along with the altitude.
+#keys must be from the same Arduino so that all the data was logged together.
 def filter_file(filename, keys_to_include):
+    #open the base file to use as the central data soource and create a new file
     with open (filename, 'r') as original_file, open(str(uuid.uuid4()) + '.csv', 'w') as out_file:
-        global previous_altitude
+        #create a variable called previous altitude to store the last altitude found.
+        previous_altitude = -1 
+        #open the original file as a list of dictionaries.
         reader = csv.DictReader(original_file)
+        #create a variable called all_csv_keys to use as the columns for the output csv.
+        #use time and altitude in in addition to the specified keys
         all_csv_keys = ['time', 'altitude']
+        #add the specified keys into the list of keys for the outputted CSV.
         all_csv_keys.extend(keys_to_include)
+        #create a csv using the newly created file with the columns in all_csv_keys
         writer = csv.DictWriter(out_file, all_csv_keys)
+        #write the header row (the column names)
         writer.writeheader()
+        #iterate through the original base file.
         for row in reader:
+            #if an altitude is present in this row
             if row['calculated_altitude'] is not None and len(row['calculated_altitude']) > 0:
+                #set previous_altitude equal to the altitude found
                 previous_altitude = row['calculated_altitude']
-            print previous_altitude
+
+            #create a dictionary to add to the csv with the timestamp of the current row
+            #and altitude to the most recently found altitude
             dictionary = {
                 'time': row['time'],
                 'altitude': previous_altitude
             }
+            #create a variable called error_found to set to True if this row does
+            #not contain all of the keys in keys_to_include
             error_found = False
+            #iterate through the keys that we want to add the altitude for.
             for key in keys_to_include:
+                #set the variable val equal to the cell in the current row with the column name key
                 val = row[key]
-                if val is not None and len(str(val)) > 0:
+                #check if there is any measurement in the cell.
+                if val is not None and len(str(val)) > 0: 
+                    #add the measurement into the dictionary to write into a new CSV.
                     dictionary[key] = val
-                else:
-                    error_found = True
-            if error_found == False:
-                writer.writerow(dictionary)
+                else: #no measurement for the cell
+                    error_found = True #set error_found to False to indicate that a key was missing at this row.
+            if error_found == False: #if an error was not found
+                writer.writerow(dictionary) #write the dictionary into the output CSV.
 
 
 
-
-#addAltitudeIfPressurePresent('master_unprocessed.csv')
-#filter_file('altitude_added.csv', ['blue_voltage', 'red_voltage', 'white_voltage'])
-
-
-def filter_csv(filename, key, threshold):
-    with open(filename, 'r') as original_file, open('thresholded_' + key + '.csv', 'w') as out_file:
+#function to add altitudes to use for the error bar graphs for the pressure calibration
+#arg, filename: the filename of the CSV containing the pressures for the original and 2.5% and 97.5% CIs.
+def confidence_interval_altitude(filename):
+    #open the original file and create a new file with the altitudes
+    with open (filename, 'r') as original_file, open('altitude_' + filename, 'w') as out_file:
+        #the keys (column names) to use in the outputted CSV
+        keys = ['interior_pressure','pressure_normal','pressure_2.5','pressure_97.5', 'altitude_50', 
+        'altitude_2.5', 'altitude_97.5']
+        #open the original file a list of dictionaries.
         reader = csv.DictReader(original_file)
-        writer = csv.DictWriter(out_file, reader.fieldnames)
+        #create a new CSV out of the newly created file with the columns keys
+        writer = csv.DictWriter(out_file, keys)
+        #write the header row (the column names) of the new csv.
+        writer.writeheader()
+        #iterate through the input CSV.
+        for row in reader:
+            #calculate altitude from the 2.5% CI
+            row['altitude_2.5'] = get_altitude_from_pressure(float(row['pressure_2.5']))
+            #calculate altitude for the 97.5% CI
+            row['altitude_97.5'] = get_altitude_from_pressure(float(row['pressure_97.5']))
+            #calculate altitude for the normal calibration regression
+            row['altitude_50'] = get_altitude_from_pressure(float(row['pressure_normal']))
+            #write the row into the new CSV.
+            writer.writerow(row)
+
+
+def add_altitude_temperature(filename):
+    with open(filename, 'r') as original_file, open('altitude_' + filename, 'w') as out_file:
+        reader = csv.DictReader(original_file)
+        writer = csv.DictWriter(out_file, ['time', 'temperature', 'altitude'])
         writer.writeheader()
         for row in reader:
-            if row[key] is not None and len(row[key]) > 0 and float(row[key]) < threshold:
+            time = row['time']
+            temperature = row['calibrated_temperature']
+            altitude = get_altitude_from_pressure(float(row['pressure']))
+            writer.writerow({'time' : time, 'temperature' : temperature, 'altitude' : altitude})
+
+def summary_altitude(filename):
+    with open(filename, 'r') as original_file, open('summary_' + filename, 'w') as out_file:
+        reader = csv.DictReader(original_file)
+        writer = csv.DictWriter(out_file, ['time', 'temperature', 'altitude'])
+        last_alt = 0
+        has_reached_descent = False
+        for row in reader:
+            alt = float(row['altitude'])
+            if alt >= 20952:
+                last_alt = alt
+                has_reached_descent = True
+            if has_reached_descent == True and alt - last_alt <= -1000:
+                last_alt = alt
                 writer.writerow(row)
 
 
-#filter_csv('speed_of_sound.csv', 'sound_time', 1400)
+s#ummary_altitude('altitude_interior_temperature.csv')
+#add_altitude_temperature('interior_temperature.csv')
--- anemometer.py
+++ anemometer.py
@@ -1,40 +1,90 @@
+'''
+    File name: anemometer_correction.py
+    Author: Jason Scharff
+    Python Version: 2.7
+    Description: The anemometer rpm data was collected by recording the number
+    of rotations in a one minute period. However, during the launch, the variable
+    to keep track of rpm was never reset to 0 after each one minute interval.
+    This creates two problems:
+    1. The data demonstrated a cumulative number of spins. The data should be in rpm over a minute.
+    2. The cumulative values were so large they exceeded the max 16 bit integer on the Arduino
+    (32,767) causing integer overflow errors where the cumulative number of spins was negative.
+
+    Fortunately, this data is all easily recoverable by first correcting the integer overflow error
+    and then finding the difference between the cumulative rotations at each minute.
+
+    This file provides functions to fix both of the aforementioned issues to recover the data.
+'''
+
+#import csv module to interface with the input CSVs for the anemometer data
 import csv
 
-
+#declare a variable for the max 16 bit integer (max Arduino int)
+#to correct integer overflow errors
 MAX_16_BIT_INTEGER = 2 ** 15 - 1
+#declare a variable for the minimum 16 bit integer (min Arduino int)
+#to correct integer overflow errors
 MIN_16_BIT_INTEGER = -2 ** 15
 
 
+#function to correct integer overpower errors
+#in anemometer data
+#arg filename, the filename of the raw data to correct.
 def correct_overflows(filename):
+	#open the original file
     with open(filename, 'r') as original_file:
+    	#convert the original file into a list of dictionaries.
         list_of_entries = csv.DictReader(original_file)
+        #create a new file called overflow_corrected to use for the corrected data
         with open ('overflow_corrected.csv', 'w') as new_file:
+        	#open the newly created file as a CSV with columns for time and corrected overflow anemometer_rpm
             writer = csv.DictWriter(new_file, ['time', 'anemometer_rpm'])
+            #write the header (the column names)
             writer.writeheader()
+            #iterate through the original file
             for dictionary in list_of_entries:
+            	#set variable rpm equal to the rpm in the current row.
                 rpm = int(dictionary['anemometer_rpm'])
+                #if the rpm is negative then an overflow occurred.
                 if rpm < 0:
+                	#set the rpm equal to the max integer + the difference between the value and the min value
+                	#as overflow causes a cycle.
                     rpm = MAX_16_BIT_INTEGER + (rpm - MIN_16_BIT_INTEGER)
+                #create a dictionary to write to the new CSV with the overflow corrected value and the timestamp.
                 new_dictionary = {'time' : dictionary['time'], 'anemometer_rpm' : rpm}
+                #write the new row onto the output file.
                 writer.writerow(new_dictionary)
 
 
-
-def correctLackOfReset(filename):
+#function to correct the fact that the anemometer rpm was never reset to 0
+#by finding differences between consecutive rows.
+#input file must be formatted as a CSV with columns just for time and anemometer rpm.
+def correct_lack_of_reset(filename):
+	#open the original file to correct.
     with open(filename, 'r') as original_file:
+    	#open the original file as a list of dictionaries.
         list_of_entries = csv.DictReader(original_file)
+        #create a new file called reset_corrected to use for the corrected version.
         with open ('reset_corrected.csv', 'w') as new_file:
+        	#open the newly created file as a CSV with columns for time and corrected overflow anemometer_rpm
             writer = csv.DictWriter(new_file, ['time', 'anemometer_rpm'])
+            #write the header (the column names)
             writer.writeheader()
+            #set a variable previous rpm to 0.
+            #the values are corrected by finding differences between consecutive rows, but the first row is correct
+            #as the anemometer_rpm was initialized 0 at the beginning.
             previous_rpm = 0
+            #iterate through the original dictionary.
             for dictionary in list_of_entries:
+            	#set integer rpm equal to the rpm at the current row.
                 rpm = int(dictionary['anemometer_rpm'])
+                #correct this rpm by subtracting the previous row to get the difference.
                 rpm -= previous_rpm
+                #set variable previous_rpm to the raw (uncorrected) rpm in the row
                 previous_rpm = int(dictionary['anemometer_rpm'])
+                #set the value for anemometer_rpm in the row to the corrected rpm
                 dictionary['anemometer_rpm'] = rpm
+                #write the original row to the new CSV, but with the corrected anemometer value.
                 writer.writerow(dictionary)
 
-
-
-#correctLackOfReset('overflow_corrected.csv')
 
